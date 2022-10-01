776d39a8c3801c1c9a05343f37e6f44bf37580b6
Andrej Bolecek <andrej.bolecek@solarwinds.com>
2022-10-01 21:08:39 +0200

    script improvement

--- run_test.ps1
+++ run_test.ps1
@@ -2,5 +2,13 @@
 $nunit = ".\Tools\NUnit.Console\nunit3-console.exe"
 $outDir = ".\test_results"
 $tests = ".\bin\Debug\net6.0\Homework.dll"
+#$param = "--result:test_result.html;transform=Tools\NUnit.Console\moje.xslt"
+
+$param = "--result:test_result.xml"
 # Run tests
-& $nunit $tests --work=$outDir
+& $nunit $tests $param --work=$outDir 
+
+#$result = ".\test_results\test_result.html"
+$result = ".\test_results\test_result.xml"
+
+& $result
